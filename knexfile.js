bc7a80c8e9163aa455e5b1078e58730c3d50b79f
Bolade Akinniyi <boladebode@gmail.com>
2021-03-16 15:30:58 +0100

    latest changes

--- knexfile.js
+++ knexfile.js
@@ -36,6 +36,28 @@ module.exports = {
 
  production: {
     client: 'pg',
+    connection: {     
+      host: 'ec2-54-247-158-179.eu-west-1.compute.amazonaws.com',
+      user: 'sikaktfchrwgxt',
+      port: 5432,
+      password: '48f4642db8f7473f34c672d2f87b6116f2200e1fb116b12179c165c08f997b86',
+      database: 'dc3ikd68gimvte',
+      charset: 'utf8',
+    ssl: true,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'      
+    }
+  },
+ production1: {
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     ssl: true,
     pool: {
